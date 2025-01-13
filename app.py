865b3678b056c5820cc3ec100045675760abf2cf
Neeraj Kumar <suneerajkumar@gmail.com>
2025-01-13 16:11:17 +0530

    database reading lecture 5

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
 import sys
-
-
-
-
-
-
-
-
 
 
 
@@ -16,7 +10,8 @@ if __name__ == "__main__":
     logging.info("This is a test")
 
     try:
-        a=1/0
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("This is a custom exception")
         raise CustomException(e,sys)
---
+++ src/mlproject/utils.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+# from src.mlproject.utils import read_pgadmin_data
+
+import pandas as pd
+from dotenv import load_dotenv
+import psycopg2
+import pickle
+import numpy as np
+
+load_dotenv()
+
+database=os.environ.get("DATABASE")
+host = os.environ.get("PGHOST")
+user = os.environ.get("PGUSER")
+password = os.environ.get("PASSWORD")
+port=os.environ.get('PGPORT')
+
+
+def read_pgadmin_data():
+        # Replace these values with your actual database connection details
+    logging.info("Reading data from pgadmin4 database")
+    try:
+        mydb=psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            port=port,
+            db=database
+        )
+        logging.info(f"Connection successful: {mydb}")
+       
+        df=pd.read_sql_query('SELECT * FROM students',mydb)
+        print(df.head())
+
+        return df
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
