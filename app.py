af16d31c0208c1b1d9167586744d1867e604857e
Neeraj Kumar <suneerajkumar@gmail.com>
2025-01-13 18:48:18 +0530

    model final selected

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_tranier import ModelTrainerConfig,MOdelTrainer
 
 import sys
 
@@ -17,8 +18,12 @@ if __name__ == "__main__":
 
 
         data_transformation=DataTransformation()
-        data_transformation.intiate_data_transformation(train_data_path, test_data_path)
-        
+        train_array,test_array,_=data_transformation.intiate_data_transformation(train_data_path, test_data_path)
+
+        # model training
+        model_trainer=MOdelTrainer()
+        print(model_trainer.initiate_model_trainer(train_array, test_array))
+
     except Exception as e:
         logging.info("This is a custom exception")
         raise CustomException(e,sys)
